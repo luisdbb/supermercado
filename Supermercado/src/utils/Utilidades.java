faa3b18927b06acd877f1d69d5a2dbf31a32b092
luisdbb <luisdbb@educastur.org>
2021-11-17 17:18:48 +0100

    nuevas utils

--- Supermercado/src/utils/Utilidades.java
+++ Supermercado/src/utils/Utilidades.java
@@ -2,6 +2,7 @@ package utils;
 
 import java.sql.Date;
 import java.time.LocalDate;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Utilidades {
@@ -39,4 +40,59 @@ public class Utilidades {
         } while (!correcto);
         return ret;
     }
+    
+    /**
+     * Función que pide al usuario que introduzca 's' o 'S' para Sí o 'n' o 'N'
+     * para No y devuelve true o false en cada caso. Si el usuario no introduce
+     * ni 's' ni 'S' ni 'n' ni 'N' entonces avisa al usuario y le vuelve a pedir
+     * a que lo introduzca de nuevo.
+     *
+     * @return true si el usuario introduce 's' o 'S'; false si el usuario
+     * introduce 'n' o 'N'
+     */
+    public static boolean leerBoolean() {
+        boolean ret;
+        Scanner in;
+        char resp;
+        do {
+            System.out.println("Pulse s para Sí o n para No");
+            in = new Scanner(System.in, "ISO-8859-1");
+            resp = in.nextLine().charAt(0);
+            if (resp != 's' && resp != 'S' && resp != 'n' && resp != 'N') {
+                System.out.println("Valor introducido incorrecto.");
+            }
+        } while (resp != 's' && resp != 'S' && resp != 'n' && resp != 'N');
+        if (resp == 's' || resp != 'S') {
+            ret = true;
+        } else {
+            ret = false;
+        }
+        return ret;
+    }
+
+    /**
+     * Función que pide al usuario que introduzca un valor decimal por la
+     * entrada estándar. Si el formato introducido no es correcto, avisa al
+     * usuario y le vuelve a pedir que lo introduzca de nuevo.
+     *
+     * @return el valor double introducido por el usuario
+     */
+    public static double leerDouble() {
+        double ret = 0.0;
+        boolean correcto = false;
+        Scanner in;
+        do {
+            System.out.println("Introduzca un valor decimal (xx.xx)");
+            in = new Scanner(System.in, "ISO-8859-1");
+            try {
+                ret = in.nextDouble();
+                correcto = true;
+            } catch (InputMismatchException ime) {
+                System.out.println("Formato introducido incorrecto.");
+                correcto = false;
+            }
+        } while (!correcto);
+        return ret;
+    }
+    
 }
